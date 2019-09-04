d1688514bdf3f85fc439ae9adaa1a331f22f5f9c
Zhenge Shi <zshi0014@student.monash.edu>
2019-09-04 15:28:17 +1000

    DONE WEEK6 WORK

---
+++ FIT5032_WEEK6/Models/Sample-ownViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_WEEK6.Models
+{
+    public class Sample_ownViewModels
+    {
+        
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ FIT5032_WEEK6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_WEEK6.Startup))]
+namespace FIT5032_WEEK6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
